b7bbe260cd81107f8838424d69bced830cdf2b1c
Blessing <mbamah1999@gmail.com>
2021-12-29 17:52:54 +0000

    this should work

--- components/Book.js
+++ components/Book.js
@@ -8,7 +8,7 @@ const Book = () => {
    <section className="bg-[#FEC702] bg-[url('../public/assets/images/bg1.png')] bg-no-repeat bg-left-bottom">
      <div className="container  mx-auto px-6 py-12 flex flex-col items-center">
        <h1 className="text-3xl text-center pb-4 font-['Numans'] uppercase underline decoration-sky-500 decoration-wavy decoration-4">
-         Book of the Month
+         Book of the Monthe
        </h1>
        <h3 className="md:text-2xl text-lg text-center text-white font-['Nunito']">
          His Glory Alone - Blessing Babilah
