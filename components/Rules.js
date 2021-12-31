2f08b2ea67b7a481bb5ffd1fe0326c704594bb17
Blessing <mbamah1999@gmail.com>
2021-12-31 18:52:59 +0000

    styled the rules component and created the footer component

--- components/Rules.js
+++ components/Rules.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { images } from '../public/assets'
+import * as VsIcons from 'react-icons/vsc'
 const Rules = () => {
  return (
    <section className="container bg-white mx-auto px-6 py-12">
@@ -11,15 +12,47 @@ const Rules = () => {
        <h3 className="font-['Nunito'] text-lg text-center">
          You would share your Reading
        </h3>
-       <div className="main flex flex-col md:flex-row items-center  justify-evenly">
-         <div className="text">
-           <p>We value respect and group engagement.</p>
-           <p>
-             Open-mindedness is diamond. No racial, geographic discrimination.
-           </p>
-           <p>We share our knowledge, because that’s the whole point.</p>
+       <div className="main flex flex-col md:flex-row gap-12 items-center justify-evenly">
+         <div className="text ">
+           <ul className="space-y-3   ">
+             <li className="flex items-center text-xl">
+               <VsIcons.VscLaw /> We value respect and group engagement.
+             </li>
+             <li className="flex text-xl items-start justify-items-start ">
+               <VsIcons.VscLaw />
+               <p>
+               Open-mindedness is diamond. No racial, geographic discrimination.
+               </p>
+             </li>
+             <li className=" flex text-xl">
+               <VsIcons.VscLaw />
+               We share our knowledge, because that’s the whole point.
+             </li>
+             <li className=" flex text-xl ">
+               <VsIcons.VscLaw />
+               <p>
+
+               We share our knowledge, because that’s the whole point.
+               </p>
+             </li>
+             <li className=" flex text-xl">
+               <VsIcons.VscLaw />
+               We share our knowledge, because that’s the whole point.
+             </li>
+             <li className=" flex text-xl">
+               <VsIcons.VscLaw />
+               We share our knowledge, because that’s the whole point.
+             </li>
+             <li className=" flex text-xl">
+               <VsIcons.VscLaw />
+               We share our knowledge, because that’s the whole point.
+             </li>
+           </ul>
          </div>
+         <div className='border-red-500 border-2'>
+
          <Image src={images.girl} alt="rules" />
+         </div>
        </div>
      </div>
    </section>
--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Hero from './Hero'
 import Cludruns from './Cludruns'
 import Book from './Book'
 import Rules from './Rules'
+import Footer from './Footer'
 
-export{Header, Hero, Cludruns, Book, Rules}
+export{Header, Hero, Cludruns, Book, Rules, Footer}
